d64765e578f1018bd54ec7270ff7c241688bf046
ZA <za@ZAs-MacBook-Pro.local>
2020-12-16 10:03:40 -0600

    Creating Model, Migration, Contoller and Fctoies

---
+++ database/factories/Model/LikeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Model;
+
+use App\Models\Model\Like;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LikeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Like::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/Model/QuestionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Model;
+
+use App\Models\Model\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Question::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
